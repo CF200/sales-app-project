87f5da3c141afcd841e0b2399798b68a0fd70d79
unknown <odeth18@hotmail.es>
2023-11-12 15:14:25 -0500

    Initialize Data: [Sales Module] [Form v1.0]

---
+++ src/app/(DashboardLayout)/layout/sidebar/MenuItems.tsx
@@ -0,0 +1,28 @@
+import {
+  IconBoxMultiple, IconCircleDot, IconHome, IconInfoCircle, IconLayout, IconLayoutGrid, IconPhoto, IconPoint, IconStar, IconTable, IconUser
+} from "@tabler/icons-react";
+
+import { uniqueId } from "lodash";
+
+const Menuitems = [
+  {
+    id: uniqueId(),
+    title: "Dashboard",
+    icon: IconHome,
+    href: "/",
+  },
+  {
+    id: uniqueId(),
+    title: "Forms",
+    icon: IconTable,
+    href: "/ui-components/sale",
+  },
+  {
+    id: uniqueId(),
+    title: "Formulario",
+    icon: IconTable,
+    href: "/ui-components/formulario",
+  }
+];
+
+export default Menuitems;
